fa2859527184989c9fbcb09df2337b76dcda39fd
Prabhu, Manohar <pmanohar@medassets.com>
2015-06-03 10:22:28 -0500

    First Commit

---
+++ EmployeeWellnessAPI/routes/users.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/users', function (req, res) {
+    res.send('users',new {title:"resource"});
+});
+
+module.exports = router;
